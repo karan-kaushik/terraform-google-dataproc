ce0ff0757d7010c5e3cd3a11fbdb6f67c6044c5b
Karan Kaushik <karan.kaushik@searce.com>
2020-11-09 15:33:26 +0530

    add file for declaring env variables

---
+++ env-variable.sh
@@ -0,0 +1,5 @@
+sudo curl -sSO https://dl.google.com/cloudagents/add-monitoring-agent-repo.sh
+sudo bash add-monitoring-agent-repo.sh
+sudo apt-get update
+sudo apt-get install stackdriver-agent -y
+sudo service stackdriver-agent start
